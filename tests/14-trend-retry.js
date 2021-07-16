445f6fd8d792180ef32406a5d56999516c983339
giovani.mussio <zeuser@ip-192-168-15-3.us-west-2.compute.internal>
2021-07-16 13:46:56 -0300

    Trend retries

---
+++ tests/14-trend-retry.js
@@ -0,0 +1,30 @@
+import {Counter,Trend} from 'k6/metrics'
+var retryCounter = new Counter("GetAPI_MAX_RETRY")
+import http from 'k6/http'
+import { sleep } from 'k6'
+
+
+var retryTrend = new Trend('GETAPI_MAX_RERY_TREND')
+
+export default function(){
+    //so retry the API call
+    var maxAttemps = 5
+    retryCounter.add(1)
+    for(var retries = 5; retries>0; retries--){
+        var numeberOfAttempts = maxAttemps-retries+1
+        retryTrend.add(numeberOfAttempts)
+        const response = http.get("https://run.mocky.io/v3/a400c4fc-2220-4dd1-93a8-1ca20a3e9026");
+        //assumes this api returns 404
+        if(response.status !== 404){
+            retryCounter.add(1)
+            console.log (`response is not correct, attempt is ${numeberOfAttempts} VU=${__VU} ITER=${__ITER} sleeping for 1 second`)
+            // so e need to retry after every 1 second
+            sleep(1)
+        }else{
+            //response is correct, retries ==0
+            retries == 0
+
+        }
+    }
+
+}
